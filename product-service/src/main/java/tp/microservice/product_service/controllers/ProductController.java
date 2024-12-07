3cc62cbed3c92528238e0d423572cad148386747
Alaa Brahim <alaa.brahim@etudiant-fst.utm.tn>
2024-12-07 10:00:16 +0100

    Implement CQRS pattern for product service with command and event handling

--- product-service/src/main/java/tp/microservice/product_service/controllers/ProductController.java
+++ product-service/src/main/java/tp/microservice/product_service/controllers/ProductController.java
@@ -1,12 +1,15 @@
 package tp.microservice.product_service.controllers;
 
+import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import tp.microservice.product_service.cqrs.commands.CreateProductCommand;
 import tp.microservice.product_service.entities.Product;
 import tp.microservice.product_service.services.ProductService;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/products")
@@ -14,10 +17,16 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+    @Autowired
+    private CommandGateway commandGateway;
 
     @PostMapping
-    public Product createProduct(@RequestBody Product product) {
-        return productService.createProduct(product);
+    public String createProduct(@RequestBody Product product) {
+        String id = UUID.randomUUID().toString();
+        CreateProductCommand command = new CreateProductCommand(
+                id, product.getName(), product.getDescription(), product.getPrice());
+        commandGateway.sendAndWait(command);
+        return id;
     }
 
     @GetMapping("/{id}")
